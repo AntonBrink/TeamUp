0e24fd69209a767b39ca23ba370f6bcd2f05510f
Anton Brink <twiggymocha@gmail.com>
2021-10-03 18:46:30 +0200

    added some funcionality to view teams page, must still add role and year filtering and request join button and functionality

--- pages/view_team.js
+++ pages/view_team.js
@@ -1,6 +1,5 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/viewTeams.module.css";
 import Link from "next/link";
-
 
 import React, { useContext, useEffect, useState } from "react";
 import AuthContext from "../stores/authContext";
@@ -9,6 +8,27 @@ export default function Home() {
   const { user, authReady } = useContext(AuthContext);
   const [teams] = useState([]);
   const [teamsReady, setTeamsReady] = useState(false);
+  const [teamName, setTeamName] = useState("");
+  const [description, setDescription] = useState("");
+  const [names, setNames] = useState([]);
+  const [date, setDate] = useState("");
+  const [goAhead, setGoAhead] = useState(false);
+
+  const setData = (id) => {
+    setNames([]);
+
+    teams.map((team) => {
+      if (team.id == id) {
+        setTeamName(team.teamName);
+        setDate(team.applyEndDate);
+        setDescription(team.groupDescription);
+        team.memberData.forEach((member) => {
+          console.log(member);
+          setNames((names) => [...names, member.name]);
+        });
+      }
+    });
+  };
 
   useEffect(() => {
     if (authReady && user) {
@@ -24,6 +44,10 @@ export default function Home() {
   teams {
     teamName
     id
+    openPositions
+    memberData
+    applyEndDate
+    groupDescription
   
 }
 
@@ -36,7 +60,14 @@ export default function Home() {
         .then((res) => res.json())
         .then((result) => {
           result.data.teams.forEach((team) => {
-            teams.push(<p key={team.id}>{team.teamName}</p>);
+            teams.push({
+              id: team.id,
+              teamName: team.teamName,
+              memberData: team.memberData,
+              openPositions: team.openPositions,
+              applyEndDate: team.applyEndDate,
+              groupDescription: team.groupDescription,
+            });
           });
           setTeamsReady(true);
         });
@@ -45,7 +76,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1 >Homepage</h1>
+      <h1>Homepage</h1>
       <div>
         <div>
           <label htmlFor="">Role/Degree</label>
@@ -63,18 +94,68 @@ export default function Home() {
             <option value=""></option>
           </select>
         </div>
-        <div>
-          <Link href="manage_page">View Team(s)</Link>
-          <Link href="create_team">Create Team</Link>
-        </div>
       </div>
 
       {!authReady && <h1>Loading...</h1>}
       {!teamsReady && <h1>Loading teams...</h1>}
 
-      {teams.map((team, id) => {
-        return <h1 key={id}>{team}</h1>;
-      })}
+      <div className={styles.pageContent}>
+        <div className={styles.teams}>
+          {teams.map((team) => {
+            let totalPositions = 0;
+
+            team.openPositions.forEach((openPosition) => {
+              totalPositions += parseInt(openPosition.Amount);
+            });
+
+            return (
+              <button
+                key={team.id}
+                onClick={() => {
+                  setData(team.id);
+                }}
+              >
+                <div>
+                  <h1> {team.teamName}</h1>
+                  <p>
+                    {team.memberData.length}/{totalPositions} members
+                  </p>
+                  <ul>
+                    Member(s) needed:
+                    {team.openPositions.map((openPosition, id) => {
+                      return (
+                        <li key={id}>
+                          {openPosition.Amount} {openPosition.Role}(s)
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </button>
+            );
+          })}
+        </div>
+
+        <div className={styles.teamInfo}>
+          <h1>{teamName}</h1>
+          <div>
+            <p></p>
+            <ul>
+              {names.map((name, id) => {
+                return <li key={id}>{name}</li>;
+              })}
+            </ul>
+            <div>
+              <h2>Team Description:</h2>
+              <p>{description}</p>
+            </div>
+            <div>
+              <h2>Applications Closing Date</h2>
+              <p>{date}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
